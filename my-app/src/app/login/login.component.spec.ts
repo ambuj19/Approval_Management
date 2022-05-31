65def0817ad7b30574693e58e701dc272ef61fc0
Kumar.Ambuj <ambujsrivastava092@gmail.com>
2022-05-31 15:46:07 +0530

    Updated Code

--- my-app/src/app/login/login.component.spec.ts
+++ my-app/src/app/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,6 +9,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -21,5 +23,14 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  fit('Button click event using spyon', () => {
+    spyOn(component, 'navigation');
+    component.navigation();
+
+    fixture.detectChanges();
+
+    let buton = fixture.debugElement.query(By.css('button')).nativeElement.click();
+    expect(component.navigation).toHaveBeenCalled();
   });
 });
--- my-app/src/app/request.service.spec.ts
+++ my-app/src/app/request.service.spec.ts
@@ -1,16 +1,52 @@
 import { TestBed } from '@angular/core/testing';
-
+import {HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { RequestService } from './request.service';
-
+import { request } from './Model/request.model';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 describe('RequestService', () => {
   let service: RequestService;
-
+  let httpClient: HttpClient;
+ const baseUrl='http://localhost:3000/request';
+  let httpTestingController: HttpTestingController;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        RequestService,
+        { provide: 'http://localhost:3000/request',
+          useValue: 'apiurl'
+        }],
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.get(HttpClient);
     service = TestBed.inject(RequestService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should get all request', () => {
+    const Mockrequestdata=[
+      {
+      id: 13,
+      purpose: "Trip",
+      description: "Trip with team to Pondicherry",
+      Approver: "Kaushal",
+      Approved: true,
+      Reject: false,
+      Estimated: 100000,
+      Advance: 50000,
+      date: "2022-05-25"
+    }
+  ];
+    let response:request[];
+
+     service.getrequest().subscribe(res => {
+
+        response=res;
+        // expect(response).toEqual(Mockrequestdata)
+     });
+     const req = httpTestingController.expectOne(baseUrl);
+     debugger;
+    expect(req.request.method).toBe('GET');
+     req.flush(Mockrequestdata);
+     httpTestingController.verify();
   });
 });
